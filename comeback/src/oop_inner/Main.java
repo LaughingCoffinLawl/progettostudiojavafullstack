4c910f3307c408de1214e32ff5db2c7bc16eca43
LaughingCoffinLawl <Veglia Marco@LAPTOP-0T8S5MBH.homenet.telecomitalia.it>
2022-10-17 20:58:02 +0200

    0.1.1

---
+++ comeback/src/oop_inner/Main.java
@@ -0,0 +1,10 @@
+package oop_inner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Door door = new Door();
+		System.out.println(door.isLocked("qwerty"));
+ 
+	}
+}
---
+++ comeback/src/ui_try/Try.java
@@ -0,0 +1,18 @@
+package ui_try;
+
+public class Try {
+
+	public static void main(String[] args) {
+		int x = 0;
+		try{
+		
+		    System.out.println("We don't know, what will be the outcome "+x/0);
+		
+		}catch (ArithmeticException e) {
+			System.out.println("Arithmetic Exception occurred");
+		}
+		catch (RuntimeException e) {
+			System.out.println("Inside exception block");
+		}
+	}
+}
