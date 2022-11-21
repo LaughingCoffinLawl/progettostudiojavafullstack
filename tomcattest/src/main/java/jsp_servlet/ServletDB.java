5212572ce7d298b034e5ca9deda5f24f8f1c0bb1
LaughingCoffinLawl <electro600@live.it>
2022-11-21 23:12:44 +0100

    JSP DB V.0.0.2 fixed

--- tomcattest/src/main/java/jsp_servlet/ServletDB.java
+++ tomcattest/src/main/java/jsp_servlet/ServletDB.java
@@ -1,47 +1,44 @@
 package jsp_servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
 import javax.annotation.Resource;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import javax.sql.DataSource;
 
-import org.apache.jasper.tagplugins.jstl.core.Out;
-
-import com.mysql.cj.xdevapi.Statement;
 
 /**
  * Servlet implementation class ServletDB
  */
-@WebServlet("/ServletDB")
+@WebServlet("/Test")
 public class ServletDB extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private DataSource dataSource;
 	@Resource(name="jdbc/project")
+	private DataSource dataSource;
 	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
 		Connection connect = null;
 		Statement stmt = null;
 		ResultSet rs = null;
 		
 		try {
 			connect = dataSource.getConnection();
-			String query = "Select * from users";
-			stmt = (Statement) connect.createStatement();
-			rs = (ResultSet) ((java.sql.Statement) stmt).executeQuery(query);
+			out.print("Connection Established!");
+			String query = "Select * FROM my_db.users";
+			stmt = connect.createStatement();
+			rs = stmt.executeQuery(query);
 			while(rs.next()) {
-				
+				out.print("<br>" + rs.getString("email"));
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
